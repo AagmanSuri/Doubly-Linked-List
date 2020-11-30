cb29fa18b31773557aa5bbf5a4e86f919611c2f1
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-11-30 18:50:07 +0530

    Creation display doubly linked list

--- Doubly Linked List/main.c
+++ Doubly Linked List/main.c
@@ -1,14 +1,45 @@
-//
-//  main.c
-//  Doubly Linked List
-//
-//  Created by Aagman Suri on 30/11/20.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+
+void create(int A[],int n)
+{
+    int i;
+    struct Node *t,*last;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+    
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+}
+void Display(struct Node *p)
+{
+    while(p)
+    {
+        printf("%d ", p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    int A[]={1,2,3,4,5};
+    create(A, 5);
+    Display(first);
 }
