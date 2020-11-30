ef66aa83fe88d7ac2d52c34fa0655ad080b1f11d
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-11-30 19:46:40 +0530

    Inserting

--- Doubly Linked List/main.c
+++ Doubly Linked List/main.c
@@ -47,10 +47,46 @@ int length(struct Node *p)
     }
     return len;
 }
+void Insert(struct Node *p,int index ,int x)
+{
+    struct Node *t;
+    int i;
+    
+    if(index<0 || index>length(p))
+    {
+        return;
+    }
+    if(index==0)
+    {
+        
+        t=(struct Node*)malloc(sizeof(struct Node));
+        t->data=x;
+        t->prev=NULL;
+        t->next=first;
+        first->prev=t;
+        first=t;
+    }
+    else
+    {
+        for(i=0;i<index-1;i++)
+        {
+            p=p->next;
+        }
+        t=(struct Node*)malloc(sizeof(struct Node));
+        t->data=x;
+        t->prev=p;
+        t->next=p->next;
+        if(p->next){
+            p->next->prev=t;
+        }
+        p->next=t;
+    }
+}
 int main()
 {
     int A[]={1,2,3,4,5};
     create(A, 5);
+    Insert(first, 0, 25);
     Display(first);
     printf("Length : %d",length(first));
 }
