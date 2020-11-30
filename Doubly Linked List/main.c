7de2231a3f2ab7c63b137551bd4322f5f1217b25
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-11-30 18:17:36 +0530

    Initial Commit

---
+++ Doubly Linked List/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Doubly Linked List
+//
+//  Created by Aagman Suri on 30/11/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
